53a2b2898c1811e8d47ee40e0c22274bf04129f6
hirahara7 <hiraharaayaka7@gmail.com>
2016-10-26 10:03:43 +0900

    ㄹㄹㄹㄹ

--- app/src/main/java/controller/urlController.java
+++ app/src/main/java/controller/urlController.java
@@ -82,7 +82,7 @@ public class urlController {
 		System.out.println(client);
 		String ret = login.insert_bs_Client(client);
 		System.out.println(ret);
-		
+		System.out.println("호진진진진2");
 		return "main";
 	}
 	
